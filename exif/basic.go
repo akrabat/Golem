8928d6ea198fedbadec195ce260cf48ae909d728
Rob Allen <rob@akrabat.com>
2020-05-30 12:30:06 +0100

    Enhance GetBasicInfo
    Added DateCreated, Make, Model, ShutterSpeed, Aperture and ISO. Ensured
    that they and the initial properties all worked.

--- exif/basic.go
+++ exif/basic.go
@@ -2,22 +2,29 @@ package ImgMeta
 
 import (
 	"fmt"
+	"math"
 )
 
 // BasicInfo contains the most basic information that could be asked for
 type BasicInfo struct {
-	Width    interface{}
-	Height   uint32
-	Title    string
-	Descr    string
-	Keywords []string
+	Width        uint32
+	Height       uint32
+	Title        string
+	Descr        string
+	Keywords     []string
+	DateCreated  string
+	Make         string
+	Model        string
+	ShutterSpeed string
+	Aperture     string
+	ISO		     string
 }
 
 // GetBasicInfo gets the basic information from the meta-information of the image
 func GetBasicInfo(img Image) (info BasicInfo) {
 	width, err := img.ReadTagValue("SOF0", SOF0ImageWidth)
 	if err == nil {
-		info.Width = width
+		info.Width = width.(uint32)
 	} else {
 		fmt.Println(err.Error())
 	}
@@ -27,19 +34,61 @@ func GetBasicInfo(img Image) (info BasicInfo) {
 	} else {
 		fmt.Println(err.Error())
 	}
-	keyword, err := img.ReadTagValue("IPTC", IptcTagApplication2Keywords)
+
+	title, err := img.ReadTagValue("IPTC", IptcTagApplication2ObjectName)
 	if err == nil {
-		info.Keywords = []string{keyword.(string)}
+		info.Title = title.(string)
 	}
+
+	desc, err := img.ReadTagValue("IPTC", IptcTagApplication2Caption)
+	if err == nil {
+		info.Descr = desc.(string)
+	}
+
+	keywords, err := img.ReadTagValue("IPTC", IptcTagApplication2Keywords)
+	if err == nil {
+		info.Keywords = keywords.([]string)
+	}
+
 	datetime, err := img.ReadTagValue("EXIF", ExifTagDateTimeOriginal)
 	if err == nil {
-		fmt.Printf("datetime:%v\n", datetime)
+		info.DateCreated = datetime.(string)
 	}
-	//height, err := img.ReadTagValue("IPTC", IptcTagApplication2Keywords)
-	//if err == nil {
-	//	info.Height = height.(float64)
-	//} else {
-	//	fmt.Println(err.Error())
-	//}
+
+	make, err := img.ReadTagValue("EXIF", ExifTagMake)
+	if err == nil {
+		info.Make = make.(string)
+	}
+
+	model, err := img.ReadTagValue("EXIF", ExifTagModel)
+	if err == nil {
+		info.Model = model.(string)
+	}
+
+	shutterSpeed, err := img.ReadTagValue("EXIF", ExifTagExposureTime)
+	if err == nil {
+		s := shutterSpeed.(float64)
+		if s >= 1 {
+			info.ShutterSpeed = fmt.Sprintf("%.0f", s)
+		} else {
+			info.ShutterSpeed = fmt.Sprintf("1/%.0f", (1.0 / s))
+		}
+	} else {
+		// try the other one
+		shutterSpeed, err = img.ReadTagValue("EXIF", ExifTagShutterSpeedValue)
+		info.ShutterSpeed = fmt.Sprintf("1/%.0f", math.Exp2(shutterSpeed.(float64)))
+	}
+
+	aperture, err := img.ReadTagValue("EXIF", ExifTagFNumber)
+	if err == nil {
+		info.Aperture = fmt.Sprintf("%.1f", aperture.(float64))
+	}
+
+
+	iso, err := img.ReadTagValue("EXIF", ExifTagPhotographicSensitivity)
+	if err == nil {
+		info.ISO = fmt.Sprintf("%v", iso)
+	}
+
 	return
 }
